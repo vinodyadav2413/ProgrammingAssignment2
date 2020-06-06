24c3ec8fcf4c759b6242afa1b8854d15894c682f
Vinodyadav2413 <vinodyadavkannaboina@gmail.com>
2020-06-06 22:27:38 +0530

    first commit on cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL                             ## initialize inv as NULL,holds inverse value 
+  set <- function(y) {                    ##  set function to assign new value
+    x <<- y                             ## value of matrix in parent environment
+    inv <<- NULL                        ## if matrix exists, assign NULL to inv
+  }
+  get <- function(){ x}                     ## get fucntion - returns x
+  
+  setinverse <- function(inverse) {inv <<- inverse } ## assigns value of inv in parent environment
+  getinverse <- function() {inv }                    ## returns inv 
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
+  ## to the functions with the $ operator
 
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+
